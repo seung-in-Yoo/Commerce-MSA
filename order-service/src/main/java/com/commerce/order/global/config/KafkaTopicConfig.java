499bd3936b7e20cef351c942cf4aa9a2140c390d
yooseungin <tmddls91@inha.edu>
2026-06-16 15:10:32 +0900

    refactor: KafkaTopicConfig에 오케스트레이션 채널 4개 추가

--- order-service/src/main/java/com/commerce/order/global/config/KafkaTopicConfig.java
+++ order-service/src/main/java/com/commerce/order/global/config/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.commerce.order.global.config;
 
 import com.commerce.order.messaging.OrderEventPublisher;
+import com.commerce.order.messaging.SagaTopics;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,40 @@ public class KafkaTopicConfig {
     @Bean
     public NewTopic orderEventsTopic() {
         return TopicBuilder.name(OrderEventPublisher.ORDER_EVENTS_TOPIC)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    // 오케스트레이션 채널 4개
+    // 같은 주문의 메시지들이 순서를 지키도록 파티션은 1개
+    @Bean
+    public NewTopic paymentCommandsTopic() {
+        return TopicBuilder.name(SagaTopics.PAYMENT_COMMANDS)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic paymentRepliesTopic() {
+        return TopicBuilder.name(SagaTopics.PAYMENT_REPLIES)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic stockCommandsTopic() {
+        return TopicBuilder.name(SagaTopics.STOCK_COMMANDS)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic stockRepliesTopic() {
+        return TopicBuilder.name(SagaTopics.STOCK_REPLIES)
                 .partitions(1)
                 .replicas(1)
                 .build();
