12412e13831ceefd01c844b68a41ff6042f34d61
yooseungin <tmddls91@inha.edu>
2026-06-22 17:23:54 +0900

    feat: outbox producer 설정 환경변수화

--- order-service/src/main/java/com/commerce/order/global/config/KafkaProducerConfig.java
+++ order-service/src/main/java/com/commerce/order/global/config/KafkaProducerConfig.java
@@ -19,12 +19,25 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    @Value("${outbox.producer.acks:all}")
+    private String acks;
+    @Value("${outbox.producer.linger-ms:0}")
+    private int lingerMs;
+    @Value("${outbox.producer.batch-size:16384}")
+    private int batchSize;
+    @Value("${outbox.producer.compression-type:none}")
+    private String compressionType;
+
     @Bean
     public KafkaTemplate<String, String> outboxKafkaTemplate() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.ACKS_CONFIG, acks);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, lingerMs);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, compressionType);
 
         ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(props);
         KafkaTemplate<String, String> template = new KafkaTemplate<>(producerFactory);
